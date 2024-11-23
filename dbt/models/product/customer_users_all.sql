bca1007a5690ebe38ad61a764ce0a95bd5a4683e
heinrych <heinrych.gomes@poli.digital>
2024-11-23 19:26:11 -0300

    adição de pastas

---
+++ dbt/models/product/customer_users_all.sql
@@ -0,0 +1,6 @@
+with cte as (
+	select user_id, customer_id from {{source('redshift', 'customer_users')}}
+	union
+	select id as user_id, customer_id from {{source('redshift', 'users')}}
+)
+select * from cte
---
+++ dbt/models/product/trigger_create.sql
@@ -0,0 +1,18 @@
+WITH trigger_data AS (
+    SELECT
+        channel_id,
+        user_id,
+        contact_id,
+        phone,
+        chat_id,
+        message_id,
+        close_before,
+        result,
+        close_after,
+        customer_id,
+        date_occur
+    FROM
+        {{ source('logs_trigger', 'trigger') }}
+    limit 1
+)
+SELECT * FROM trigger_data
